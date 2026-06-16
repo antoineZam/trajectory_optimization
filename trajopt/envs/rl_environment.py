0129fea5e4474d3efec29fa6b465d3b2dc25de20
antoine.zamord <azamord1@gmail.com>
2026-06-16 15:39:07 +0200

    fixed heading normalization

--- trajopt/envs/rl_environment.py
+++ trajopt/envs/rl_environment.py
@@ -360,7 +360,8 @@ class RacingEnv(gym.Env):
 
         for i, spacing in enumerate(lookahead_spacing):
             # Get point ahead on track (wrap around for closed track)
-            ahead_idx = (closest_idx + spacing) % num_points
+            index_offset = int(spacing / self.track.interpolation_resolution)
+            ahead_idx = (closest_idx + index_offset) % num_points
             ahead_point = centerline[ahead_idx]
 
             # Vector from vehicle to lookahead point
@@ -440,9 +441,8 @@ class RacingEnv(gym.Env):
 
         # Heading error (as cosine: 1 = aligned, -1 = opposite direction)
         heading_diff = s.yaw - track_state["track_heading"]
-        heading_diff_normalized = heading_diff / np.pi
         heading_diff = np.arctan2(np.sin(heading_diff_normalized), np.cos(heading_diff_normalized))
-        heading_error_normalized = np.cos(heading_diff_normalized)  # [−1, 1]
+        heading_error_normalized = heading_diff / np.pi  # [−1, 1]
 
         # Track progress
         track_progress = track_state["track_progress"]
